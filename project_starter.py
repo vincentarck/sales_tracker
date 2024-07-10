27a525349cccc2f7bfa77d754fc0978b50327f67
Vincent laurensius <vincenthambaya@gmail.com>
2024-07-10 13:47:47 +0700

    chore(mappig): relational data within requiremends

---
+++ project_starter.py
@@ -0,0 +1,26 @@
+relation_of_products_name = {
+    "P001": "Wireless Headphones",
+    "P002": "Laptop Backpack" ,
+    "P003": "Bluetooth Speaker" ,
+    "P004": "USB Flash Drive", 
+    "P005": "Mobile Phone Case",
+    "P006": "Wireless Mouse" ,
+    "P007": "Laptop Stand" ,
+    "P008": "HDMI Cable" ,
+    "P009": "Smartphone",
+    "P010": "External Hard Drive"
+}
+
+relation_of_products_price = {
+    "Wireless Headphones": 100,
+    "Laptop Backpack": 60 ,
+    "Bluetooth Speaker": 50 ,
+    "USB Flash Drive": 20, 
+    "Mobile Phone Case": 15,
+    "Wireless Mouse": 30 ,
+    "Wireless Mouse": 40 ,
+    "HDMI Cable": 15 ,
+    "Smartphone": 600,
+    "Smartphone": 100
+}
+
